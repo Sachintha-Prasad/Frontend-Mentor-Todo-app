aa4b2b013af6f106c154afd07caaeff10a5a65c2
Sachintha Prasad <prasadsachintha1231@gmail.com>
2024-10-26 11:25:26 +0530

    config: add config files

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,25 @@
 export default {
     content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
     theme: {
-        extend: {}
+        extend: {
+            container: {
+                center: true,
+                padding: "2rem"
+            }
+        },
+
+        colors: {
+            primary: "hsl(var(--color-primary))",
+            "primary-gradient-accent-1":
+                "hsl(var(--color-primary-gradient-accent-1))",
+            "primary-gradient-accent-2":
+                "hsl(var(--color-primary-gradient-accent-2))",
+            "bg-background": "hsl(var(--color-bg-background))",
+            "bg-content": "hsl(var(--color-bg-content))",
+            "text-primary": "hsl(var(--color-text-primary))",
+            "text-secondary": "hsl(var(--color-text-secondary))",
+            "text-hover": "hsl(var(--color-bg-content))"
+        }
     },
     plugins: []
 }
