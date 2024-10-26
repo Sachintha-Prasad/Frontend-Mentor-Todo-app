c4c10990865c7635a77d18e9971b1cb0ca0b71e6
Sachintha Prasad <prasadsachintha1231@gmail.com>
2024-10-27 01:22:36 +0530

    feat(store): add task reducer

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit"
 import themeReducer from "../features/theme/themeSlice"
+import tasksReducer from "../features/tasks/taskSlice"
 
 export const store = configureStore({
     reducer: {
-        themeReducer: themeReducer
+        themeReducer: themeReducer,
+        tasksReducer: tasksReducer
     }
 })
 
