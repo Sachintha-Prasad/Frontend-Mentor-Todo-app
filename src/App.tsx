98e75a7fb00576be25239d296385159f294d72d0
Sachintha Prasad <prasadsachintha1231@gmail.com>
2024-10-26 11:26:59 +0530

    refactor: update file

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,37 @@
+import ThemeToggler from "./features/theme/ThemeToggler"
+import { useAppSelector } from "./hooks/useAppSelector"
+
 const App = () => {
-    return <div>App</div>
+    const themeMode = useAppSelector((state) => state.themeReducer.themeMode)
+
+    return (
+        <div className="h-screen bg-bg-background">
+            <div
+                className={`w-screen h-[40%]  ${
+                    themeMode === "light"
+                        ? "bg-[url(./assets/images/bg-mobile-light.jpg)]"
+                        : "bg-[url(./assets/images/bg-mobile-dark.jpg)]"
+                } ${
+                    themeMode === "light"
+                        ? "sm:bg-[url(./assets/images/bg-desktop-light.jpg)]"
+                        : "sm:bg-[url(./assets/images/bg-desktop-dark.jpg)]"
+                } bg-cover`}
+            >
+                <div className="w-full max-w-[750px] container flex flex-col gap-6 items-center justify-between absolute top-[144px] left-1/2 -translate-x-1/2">
+                    <div className="w-full flex items-center justify-between">
+                        <p className="text-[#fff] text-5xl font-semibold leading-none tracking-[8px] mt-3">
+                            TODO
+                        </p>
+                        <ThemeToggler />
+                    </div>
+                    <div className="w-full flex flex-col gap-3">
+                        <div className="w-full bg-bg-content min-h-[50px] rounded-md"></div>
+                        <div className="w-full bg-bg-content min-h-[300px] rounded-md shadow-lg"></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
 }
 
 export default App
